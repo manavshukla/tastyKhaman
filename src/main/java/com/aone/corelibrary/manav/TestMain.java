907226515fa11fd573afb96606e755ea6e5cb374
Manav shukla <manav7176@gmail.com>
2024-11-12 01:57:24 +0530

    final updates

--- src/main/java/com/aone/corelibrary/manav/TestMain.java
+++ src/main/java/com/aone/corelibrary/manav/TestMain.java
@@ -2,11 +2,13 @@ package com.aone.corelibrary.manav;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/hello")
 public class TestMain {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String home() {
         return "Hello manav";
     }
